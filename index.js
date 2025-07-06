1fd818df11f6fb4047acf7208ff2a191442e28fd
Md Ashif Iqbal <contact2ashif@gmail.com>
2025-07-06 22:48:48 +0530

    Added isAbove18 fn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const name3 = "Rahul";
 function sayName() {
   return name3;
 }
+function isAbove18() {
+  return age >= 18;
+}
--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,5 @@
 function add(num2, num2) {
   return num1 + num2;
 }
+
+console.log("Hello");
