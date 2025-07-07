01f840ea21d0c67fc9cb82c4178a56c4e4be70cb
Md Ashif Iqbal <contact2ashif@gmail.com>
2025-07-07 14:10:29 +0530

    Now added scipt.js as well in the commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function sayHello() {
   return "Hello";
 }
+
+console.log(
+  "I am going to put utils.js in the staging area and leave script.js"
+);
--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,4 @@
-function add(num2, num2) {
+function add(num1, num2) {
   return num1 + num2;
 }
 
@@ -7,3 +7,5 @@ console.log("Hello");
 console.log(
   "I am going to put utils.js in the staging area and leave script.js"
 );
+
+console.log("This is the ammended commit");
